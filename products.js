db5a9c384595cc98baec3b3ab05e8bf73fc06d31
samuel119792 <samuelaraujoaraujo06@gmail.com>
2025-11-24 21:57:41 -0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,8 @@
+const products = [
+    { id: 1, name: "Teclado Mecânico RGB", price: 199.99, image: "img1" },
+    { id: 2, name: "Mouse Gamer 7200 DPI", price: 79.99, image: "img2" },
+    { id: 3, name: "Headset Surround 7.1", price: 159.99, image: "img3" },
+    { id: 4, name: "SSD 480GB", price: 129.99, image: "img4" },
+    { id: 5, name: "Placa de Vídeo GTX 1650", price: 899.99, image: "img5" },
+    { id: 6, name: "Fonte 600W", price: 229.99, image: "img6" }
+];
